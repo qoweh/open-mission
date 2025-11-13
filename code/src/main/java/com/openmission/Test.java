6616378f70219c40797edbefb5ce3fec76df0a04
MINWOO KIM <rlaalsdn110@gmail.com>
2025-11-13 23:49:27 +0900

    feat: 여러명에게 메일 발송 API 테스트

--- code/src/main/java/com/openmission/Test.java
+++ code/src/main/java/com/openmission/Test.java
@@ -1,6 +1,7 @@
 package com.openmission;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.mail.Address;
 import jakarta.mail.Authenticator;
 import jakarta.mail.Message;
 import jakarta.mail.PasswordAuthentication;
@@ -8,6 +9,8 @@ import jakarta.mail.Session;
 import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Test {
@@ -27,7 +30,6 @@ public class Test {
         String receiver = "sunnybikers@naver.com";
         String title = "테스트 메일입니다.";
         String content = "안녕하세요 ㅎㅇㅎㅇ";
-
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -36,7 +38,8 @@ public class Test {
         });
         Message message = new MimeMessage(session);
         try {
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+            message.addRecipients(Message.RecipientType.TO, new InternetAddress[] {new InternetAddress("minu421@naver.com"), new InternetAddress("sunnybikers@naver.com")});
             message.setSubject(title);
             message.setContent(content, "text/html; charset=utf-8");
 
